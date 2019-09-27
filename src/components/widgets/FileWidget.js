b1ccfc3d15293c06b09178b20af066d8ea03b0c0
shyamvb <shyam.vb@gmail.com>
2019-09-27 05:47:07 +0530

    added rodal support

--- src/components/widgets/FileWidget.js
+++ src/components/widgets/FileWidget.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Rodal from "rodal";
-import "rodal/lib/rodal.css";
 
 import { dataURItoBlob, shouldRender } from "../../utils";
 
