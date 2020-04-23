fe2259616d6a0afd3b3403508d41a2b7327652ab
Sachin-thoughti <46992794+Sachin-thoughti@users.noreply.github.com>
2020-04-23 14:57:45 +0530

    Update Form.js file to hide submit button
    Update Form.js to hide submit button

--- src/components/Form.js
+++ src/components/Form.js
@@ -365,7 +365,7 @@ export default class Form extends Component {
           children
         ) : (
           <div>
-            <button type="submit" className="btn btn-info">
+            <button type="submit" className="btn btn-info" style={{display:'none'}}>
               Submit
             </button>
           </div>
